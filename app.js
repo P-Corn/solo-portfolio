8f61315c76f16a8d80b0dcac088ed29df62476bb
Peyton Cornelison <peytoncornelison@gmail.com>
2021-04-29 21:11:08 -0500

    added modal skeleton for the project section to display project information

---
+++ app.js
@@ -0,0 +1,11 @@
+const modalOpenBtn = document.querySelector('.modal-btn');
+const modalBg = document.querySelector('.modal-bg');
+const modalCloseBtn = document.querySelector('.modal__close-btn');
+
+modalOpenBtn.addEventListener('click', () => {
+    modalBg.classList.add('show-modal');
+})
+
+modalCloseBtn.addEventListener('click', () => {
+    modalBg.classList.remove('show-modal');
+})
