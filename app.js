b01aeb17dc0495d11231bae46d70e1cc9963a3de
Peyton Cornelison <peytoncornelison@gmail.com>
2021-05-13 15:51:04 -0500

    added more time for the animation to display

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ const handleAnimation = () => {
             skillSliders[index].style.top = '0';
             setTimeout(() => {
                 skillSliders[index].style.top = '100%';
-            }, 850)
+            }, 1000)
         }, time)
         time += 70;
     })
