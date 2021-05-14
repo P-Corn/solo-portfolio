4d25883f266151ab658d390f9b328b71daba6c9a
Peyton Cornelison <peytoncornelison@gmail.com>
2021-05-13 19:16:46 -0500

    adjusted starting distance of animation for the project cards

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
 //NAV
 const linkWrap = document.querySelector('.link-wrap');
 const navIcon = document.querySelector('.nav__icon');
